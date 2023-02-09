37c145cfa40ce2b796bb1b18b0aa330c24698753
Sylvio77 <mensahsylvain@gmail.com>
2023-02-09 05:07:47 -0500

    End of section 8

--- Core/Specification/ProductSpecParams.cs
+++ Core/Specification/ProductSpecParams.cs
@@ -2,22 +2,22 @@ namespace Core.Specification
 {
     public class ProductSpecParams
     {
-        private const int MaxPageSize=50;
-        public int PageIndex {get;set;}=1;
-        private int _pageSize=6;
+        private const int MaxPageSize = 50;
+        public int PageIndex { get; set; } = 1;
+        private int _pageSize = 6;
         public int pageSize
         {
-           get=>_pageSize;
-           set=>_pageSize=(value>MaxPageSize)?MaxPageSize:value;
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
-        public int? BrandId {get;set;}
-        public int? TypeId {get;set;}
-        public string ? Sort{get;set;}
-        private string _search;
-        public string Search
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string? Sort { get; set; }
+        private string ? _search;
+        public string ? Search
         {
-            get=>_search;
-            set=>_search=value.ToLower();
+            get => _search;
+           set => _search =  value.ToLower();
         }
     }
 }
